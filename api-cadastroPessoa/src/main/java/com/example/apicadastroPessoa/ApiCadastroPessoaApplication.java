6fb59a2ef5ab012cb6ea0549d4ce489cfbea81bb
Matheus Lessa <matheus.lessa@sptech.school>
2024-05-17 12:11:36 -0300

    subindo a estrutura da api Cadastro Pessoa

---
+++ api-cadastroPessoa/src/main/java/com/example/apicadastroPessoa/ApiCadastroPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicadastroPessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroPessoaApplication.class, args);
+	}
+
+}
---
+++ api-cadastroPessoa/src/test/java/com/example/apicadastroPessoa/ApiCadastroPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicadastroPessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
